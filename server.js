85800037d04145da1ec7fc72503d796a6a1d0de1
chandan vishwakarma <chandanvishw11002@gmail.com>
2025-08-26 00:44:07 +0530

    backend deployement code is done

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
 const express = require("express");
-const app = express()
-const cookieParser = require("cookie-parser")
 const cors = require("cors");
 require("dotenv").config();
+const cookieParser = require("cookie-parser")
+const app = express();
+const path = require("path")
 
-// routes
 const authRoutes = require("./routes/authRoutes.js")
 const questionRoutes = require("./routes/questionRoutes");
 const evaluationRoutes = require("./routes/evaluateRoutes.js")
-
-// implemented routes
-
+// Middleware
+app.use(cors({
+  origin: "http://localhost:5173", // your frontend URL
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/api/questions", questionRoutes);
 app.use("/api/evaluate", evaluationRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
+  // Serve frontend
+  app.use(express.static(path.join(__dirname, "./frontend/dist")));
+  app.get("/*any", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./", "frontend", "dist", "index.html"));
+  });
+} else {
+  app.use(
+    cors({
+      credentials: true,
+    })
+  );
+}
 
 app.listen(process.env.PORT, () => {
     console.log(`server is running ${process.env.PORT}`)
